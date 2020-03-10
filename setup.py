db813221bdf94c13bce8ae537ef4cd456f18977d
vefremov <efremov.vitaly@gmail.com>
2020-03-10 12:56:12 +0700

    Enhance setup.py to publish readme

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='sqla-softdelete',
-    packages=['sqla-softdelete'],
-    version='1.0',
+    name='sqla_softdelete',
+    packages=['sqla_softdelete'],
+    version='1.2',
     license='GPLv3+',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='SQLAlchemy soft delete',
     author='Vitaly Efremov',
     author_email='efremov.vitaly@gmail.com',
     url='https://github.com/vitaly-efremov/sqla-softdelete',
-    download_url='https://github.com/vitaly-efremov/sqla-softdelete/archive/1.0.tar.gz',
+    download_url='https://github.com/vitaly-efremov/sqla-softdelete/archive/v1.1.tar.gz',
     keywords=['SQLAlchemy', 'Soft delete'],
     install_requires=[
         'sqlalchemy',
@@ -25,5 +30,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
